c5327fa81cc0708b34b52e7157ddfdd40a3ed73b
Asif <asifssheenu@gmail.com>
2026-08-12 16:40:33 +0530

    configure CORS

--- config/settings.py
+++ config/settings.py
@@ -166,7 +166,12 @@ SIMPLE_JWT = {
 
 }
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+]
+
+CORS_ALLOW_CREDENTIALS = True
 
 STATICFILES_STORAGE = (
     "whitenoise.storage.CompressedManifestStaticFilesStorage"
