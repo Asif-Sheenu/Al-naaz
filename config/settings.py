18e754159dae783c848b218376bef5d5b1231ebb
Asif <asifssheenu@gmail.com>
2026-08-31 15:15:25 +0530

    config

--- config/settings.py
+++ config/settings.py
@@ -188,7 +188,7 @@ SIMPLE_JWT = {
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
-    "https://your-frontend-domain.com",
+    "https://al-naaz-erp.vercel.app",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
